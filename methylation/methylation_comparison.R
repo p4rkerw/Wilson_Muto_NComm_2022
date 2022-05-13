8987c6a065f04ac7404f3bd1b9678a88b2967d2a
Parker C Wilson <53058914+p4rkerw@users.noreply.github.com>
2022-05-13 16:03:53 -0500

    Update methylation_comparison.R

--- methylation/methylation_comparison.R
+++ methylation/methylation_comparison.R
@@ -33,9 +33,11 @@ dar.gr <- makeGRangesFromDataFrame(dar.df, keep.extra.columns=TRUE)
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6549146/bin/41467_2019_10378_MOESM4_ESM.xlsx
 
 # read in table and convert col to hg19 coordinates
+study_id <- "pmid31165727"
 dmr.df <- read.xlsx(here("analysis","dkd","methylation","41467_2019_10378_MOESM4_ESM.xlsx")) %>%
   dplyr::mutate(peak = paste0(Chr,"-",Position,"-",Position)) %>%
-  dplyr::select(peak, Methylation.probe)
+  dplyr::select(peak)
+dmr.df$study_id <- study_id
 dmr.df <- tidyr::separate(dmr.df, col = peak, into = c("chrom","start","end"), sep = "-")
 dmr.gr <- makeGRangesFromDataFrame(dmr.df, keep.extra.columns=TRUE)
 dmr38.gr <- liftOver(dmr.gr, ch19) %>% unlist()
@@ -49,9 +51,11 @@ over1.gr <- join_overlap_intersect(dar.gr, dmr38.gr)
 # File Name: Supplementary Data 3
 # Description: Top 471 probes that improve model of kidney function declineusing weighted regression. (a) Model is a weighted linear regression model of adjusted eGFR slope (weight = inverse variance of adjusted eGFR slope). Base model includes variables: baseline eGFR, Diabetes, and Age (base model AIC = 206). When methylation level of probe is added to base model, the following variables are also added: methylation batch, and bisulfite conversion efficiency. (b) Association determined by linear regression models adjusted for age, gender, race, diabetes, hypertension, batch, bisulfite conversion, and degree of lymphocytic infiltrate on histology.
 # read in table and convert col to hg19 coordinates
+study_id <- "pmid31165727"
 dmr.df <- read.xlsx(here("analysis","dkd","methylation","41467_2019_10378_MOESM6_ESM.xlsx")) %>%
   dplyr::mutate(peak = paste0(chr,"-",Position,"-",Position)) %>%
   dplyr::select(peak)
+dmr.df$study_id <- study_id
 dmr.df <- tidyr::separate(dmr.df, col = peak, into = c("chrom","start","end"), sep = "-")
 dmr.gr <- makeGRangesFromDataFrame(dmr.df, keep.extra.columns=TRUE)
 dmr38.gr <- liftOver(dmr.gr, ch19) %>% unlist()
@@ -63,9 +67,11 @@ over2.gr <- join_overlap_intersect(dar.gr, dmr38.gr)
 # PMID: 33933144
 # hg19
 # Supplementary Table ST3: Top-ranked differentially methylated CpG sites for matched individuals with T1DM-ESKD (n=107) vs. T1DM (n=107): FDR p≤x10-8 (Analysis 1)
+study_id <- "pmid33933144"
 dmr.df <- read.xlsx(here("analysis","dkd","methylation","13148_2021_1081_MOESM1_ESM.xlsx"), sheet = "ST3", startRow = 2) %>%
     dplyr::mutate(peak = paste0(CHR,"-",MAPINFO,"-",MAPINFO)) %>%
-    dplyr::select(peak, CpG.ID, UCSC_RefGene_Name)
+    dplyr::select(peak)
+dmr.df$study_id <- study_id
 dmr.df <- tidyr::separate(dmr.df, col = peak, into = c("chrom","start","end"), sep = "-") %>%
   dplyr::filter(chrom != "NA")
 dmr.gr <- makeGRangesFromDataFrame(dmr.df, keep.extra.columns=TRUE)
@@ -79,9 +85,11 @@ over3.gr <- join_overlap_intersect(dar.gr, dmr38.gr)
 # PMID: 33933144
 # hg19
 # Supplementary Table ST4:Top-ranked differentially methylated CpG sites for matched individuals with T1DM-ESKD (n=107) vs. T1DM (n=107): FDR p≤x10-8 and FC ±2 (Analysis 1)
+study_id <- "pmid33933144"
 dmr.df <- read.xlsx(here("analysis","dkd","methylation","13148_2021_1081_MOESM1_ESM.xlsx"), sheet = "ST4", startRow = 2) %>%
     dplyr::mutate(peak = paste0(CHR,"-",MAPINFO,"-",MAPINFO)) %>%
-    dplyr::select(peak, CpG.ID)
+    dplyr::select(peak)
+dmr.df$study_id <- study_id
 dmr.df <- tidyr::separate(dmr.df, col = peak, into = c("chrom","start","end"), sep = "-") %>%
   dplyr::filter(chrom != "NA")
 dmr.gr <- makeGRangesFromDataFrame(dmr.df, keep.extra.columns=TRUE)
@@ -95,9 +103,11 @@ over4.gr <- join_overlap_intersect(dar.gr, dmr38.gr)
 # PMID: 33933144
 # hg19
 # Supplementary Table ST11: Top-ranked differentially methylated CpG sites for individuals with T1DM-ESKD - either dialysis or transplant (n=107) vs. T1DM (n=253): FDR p≤x10-8 (Analysis 2)
+study_id <- "pmid33933144"
 dmr.df <- read.xlsx(here("analysis","dkd","methylation","13148_2021_1081_MOESM1_ESM.xlsx"), sheet = "ST11", startRow = 2) %>%
     dplyr::mutate(peak = paste0(CHR,"-",MAPINFO,"-",MAPINFO)) %>%
-    dplyr::select(peak, CpG.ID)
+    dplyr::select(peak)
+dmr.df$study_id <- study_id
 dmr.df <- tidyr::separate(dmr.df, col = peak, into = c("chrom","start","end"), sep = "-") %>%
   dplyr::filter(chrom != "NA")
 dmr.gr <- makeGRangesFromDataFrame(dmr.df, keep.extra.columns=TRUE)
@@ -111,8 +121,10 @@ over5.gr <- join_overlap_intersect(dar.gr, dmr38.gr)
 # PMID: 24098934
 # hg18
 # Supplemental Table 2 List of differentially methylated regions with p-value methylation level, genomic locus and nearest annotated transcript
+study_id <- "pmid24098934"
 dmr.df <- read.xlsx(here("analysis","dkd","methylation","gb-2013-14-10-r108-S3.xlsx"), startRow = 3) %>%
   dplyr::select(chr, start, end)
+dmr.df$study_id <- study_id
 dmr.gr <- makeGRangesFromDataFrame(dmr.df, keep.extra.columns=TRUE)
 seqlevelsStyle(dmr.gr) <- "UCSC"
 dmr38.gr <- liftOver(dmr.gr, ch18) %>% unlist()
@@ -122,4 +134,10 @@ over6.gr <- join_overlap_intersect(dar.gr, dmr38.gr)
 
 
 
+
+
+# compile the results
+dmr.compile.df <- lapply(list(over1.gr, over2.gr, over3.gr, over4.gr, over5.gr, over6.gr), function(gr) {
+  tmp <- as.data.frame(gr)
+  }) %>% bind_rows() %>% arrange(seqnames, start)
 
