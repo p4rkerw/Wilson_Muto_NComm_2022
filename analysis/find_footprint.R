860020fc8c3bebd8868564ad623a0bf92b0c6ce0
Parker C. Wilson, MD, PhD <53058914+p4rkerw@users.noreply.github.com>
2022-08-29 14:58:15 -0500

    Update find_footprint.R

--- analysis/find_footprint.R
+++ analysis/find_footprint.R
@@ -20,7 +20,7 @@
 # export LSF_DOCKER_VOLUMES="$HOME:$HOME \
 # $STORAGE1/diabneph:$HOME/project \
 # $SCRATCH1:$SCRATCH1"
-# bsub -G compute-parkerw -R 'rusage[mem=256GB]' -q general -a 'docker(p4rkerw/sctools:R4.1.0)' -o $SCRATCH1/log.find_footprint.out Rscript $SCRATCH1/dkd/analysis/find_footprint.R
+# bsub -G compute-parkerw -R 'rusage[mem=256GB]' -q general -a 'docker(p4rkerw/sctools:R4.1.0)' -o $SCRATCH1/log.find_footprint.out Rscript $SCRATCH1/Wilson_Muto_NComm_2022/analysis/find_footprint.R
 
 library(Signac) # 1.3.0
 library(Seurat) # 4.0.3
