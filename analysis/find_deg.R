eb42ed7cb505744480769e542c08ad1f8ae11388
Parker C. Wilson, MD, PhD <53058914+p4rkerw@users.noreply.github.com>
2022-08-29 14:57:58 -0500

    Update find_deg.R

--- analysis/find_deg.R
+++ analysis/find_deg.R
@@ -20,7 +20,7 @@
 # export LSF_DOCKER_VOLUMES="$HOME:$HOME \
 # $STORAGE1/diabneph:$HOME/project \
 # $SCRATCH1:$SCRATCH1"
-# bsub -G compute-parkerw -R 'rusage[mem=256GB]' -n 10 -q general -a 'docker(p4rkerw/sctools:R4.1.0)' -o $SCRATCH1/log.find_deg.out Rscript $SCRATCH1/dkd/analysis/find_deg.R
+# bsub -G compute-parkerw -R 'rusage[mem=256GB]' -n 10 -q general -a 'docker(p4rkerw/sctools:R4.1.0)' -o $SCRATCH1/log.find_deg.out Rscript $SCRATCH1/Wilson_Muto_NComm_2022/analysis/find_deg.R
 
 library(Seurat) 
 library(EnsDb.Hsapiens.v86)
